e1e95b6603e966d155a46dfa374ea3c76d897ed0
Aisling Hemlock <aisling.hemlock@gmail.com>
2023-12-19 11:25:51 +0000

    added gitignore, added type labels

--- client/app.js
+++ client/app.js
@@ -1,13 +1,11 @@
 console.log("test");
 
 async function getMessage() {
-  const res = await fetch("http://localhost:8080/message");
+  const response = await fetch("https://miny-stac.onrender.com/message");
 
   const json = await response.json();
 
   const p = document.getElementById("message");
   p.textContent = json.message;
 }
-getMessage ();
-
-
+getMessage();
